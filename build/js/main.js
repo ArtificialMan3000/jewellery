1f87d109d593d76cbfea699bd2497bacec182df0
Aleksandr <legionerab3000@yandex.ru>
2021-04-26 20:09:55 +0300

    Реализовал мобильную версию галереи

--- build/js/main.js
+++ build/js/main.js
@@ -825,15 +825,40 @@ window.gallery = (function () {
         itemSelector: '.gallery__item',
         columnWidth: 1,
         percentPosition: true,
-        initLayout: false
+        initLayout: false,
       });
+      orderItems();
       imagesLoaded(gallery.element, function () {
         gallery.masonry.layout();
       });
     };
 
+    // Упорядочивает элементы галереи для мобильной версии
+    var orderForMobile = function () {
+      var item3 = gallery.element.querySelector('.gallery__item--3');
+      var item7 = gallery.element.querySelector('.gallery__item--7');
+      gallery.element.insertBefore(item7, item3);
+    };
+
+    // Упорядочивает элементы галереи для десктопной версии
+    var orderForDesktop = function () {
+      var item7 = gallery.element.querySelector('.gallery__item--7');
+      gallery.element.append(item7);
+    };
+
+    // Упорядочивает элементы галереи в зависимости от медиа условий
+    var orderItems = function () {
+      if (window.mediaQuery.mqls.mobile.matches) {
+        orderForMobile();
+      } else {
+        orderForDesktop();
+      }
+      gallery.masonry.reloadItems();
+    };
+
     gallery.initGallery = function () {
       gallery.createMasonryGallery();
+      window.mediaQuery.watchMedia(['mobile'], orderItems);
     };
 
     return gallery;
